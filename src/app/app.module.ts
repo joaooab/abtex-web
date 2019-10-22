01fb45c603c456e9ccf819adb464325e8825a6a1
joaoab <joaoovfreitas@gmail.com>
2019-10-21 21:44:03 -0300

    refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { ComponentsModule } from './components/components.module';
 import { SnackBarMensagemModule } from './components/snack-bar-mensagem/snack-bar-mensagem.module';
 
+
 @NgModule({
   imports: [
     BrowserAnimationsModule,
